6afc18809919034d4ea9e91b43cf4d6ef79c7293
Max Rogers <maxrogerssepulveda@gmail.com>
2021-12-30 15:02:43 -0300

    netlify google auth fix

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -28,10 +28,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         .insert("after", "save[-1]", [
           {
             _key: uuidv4(),
-            userId: user.googleId,
+            userId: user?.googleId,
             postedBy: {
               _type: "postedBy",
-              _ref: user.googleId,
+              _ref: user?.googleId,
             },
           },
         ])
@@ -113,7 +113,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                 </a>
               )}
 
-              {postedBy?._id === user.googleId && (
+              {postedBy?._id === user?.googleId && (
                 <button
                   type="button"
                   className="bg-white p-2 opacity-70 hover:opacity-100 text-black font-bold test-base rounded-3xl hover:shadow-md outline-none"
